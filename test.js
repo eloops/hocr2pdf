378b9c52ae6f6b12515d3e3a8781d3d2a68eface
Eloops <stephen@eloop.id.au>
2015-09-02 13:35:12 +1000

    Minor readability changes

--- test.js
+++ test.js
@@ -23,48 +23,51 @@ var pdfFile = new PDFDocument(options)
 var stream = fs.createWriteStream(output)
 pdfFile.pipe(stream)
 
-sharp(fs.readFileSync(imageFile)).rotate(newFile.rotation).toBuffer(function(err, data, info) {
-  if (err) { throw err }
-
-  // var words = newFile.getWords()
-  // for (var x = 0; x < words.length; x++) {
-  //   // use height of bbox
-  //   pdfFile.fontSize(words[x][3])
-  //   pdfFile.font('Helvetica')
-  //   pdfFile.fillColor('black')
-  //   pdfFile.text(words[x][4], words[x][0], words[x][1]), {
-  //     width: words[x][2],
-  //     height: words[x][3],
-  //     aligh: 'centre'
-  //   }
-  // }
+sharp(fs.readFileSync(imageFile))
+  .rotate(newFile.rotation)
+  .toBuffer(function(err, data, info) {
+    if (err) { throw err }
   
-  // var paraboxes = newFile.drawParagraphBoxes()
-  // var wordboxes = newFile.drawWordBoxes()
-  // 
-  // for (var w = 0; w < wordboxes.length; w++) {
-  //   pdfFile.rect(wordboxes[w][0],wordboxes[w][1],
-  //               wordboxes[w][2],wordboxes[w][3])
-  //          .strokeColor('#ADD8E6')
-  //          .fillColor('#ADD8E6')
-  //          .lineWidth(0)
-  //          .fillOpacity(1)
-  //          .stroke()
-  // }
-  // for (var p = 0; p < paraboxes.length; p++) {
-  //   pdfFile.rect(paraboxes[p][0],paraboxes[p][1],
-  //               paraboxes[p][2],paraboxes[p][3])
-  //          .strokeColor('#90EE90')
-  //          .lineWidth(0.5)
-  //          .fillColor('#90EE90')
-  //          .fillOpacity(0.5)
-  //          .dash(6, {space: 3})
-  //          .stroke()
-  // }
-  
-  pdfFile.image(data, 0, 0, {
-    fit: [newFile.width, newFile.height]
-  })
-  
-  pdfFile.end()
-})
+    // var words = newFile.getWords()
+    // for (var x = 0; x < words.length; x++) {
+    //   // use height of bbox
+    //   pdfFile.fontSize(words[x][3])
+    //   pdfFile.font('Helvetica')
+    //   pdfFile.fillColor('black')
+    //   pdfFile.text(words[x][4], words[x][0], words[x][1]), {
+    //     width: words[x][2],
+    //     height: words[x][3],
+    //     aligh: 'centre'
+    //   }
+    // }
+    
+    // var paraboxes = newFile.drawParagraphBoxes()
+    // var wordboxes = newFile.drawWordBoxes()
+    // 
+    // for (var w = 0; w < wordboxes.length; w++) {
+    //   pdfFile.rect(wordboxes[w][0],wordboxes[w][1],
+    //               wordboxes[w][2],wordboxes[w][3])
+    //          .strokeColor('#ADD8E6')
+    //          .fillColor('#ADD8E6')
+    //          .lineWidth(0)
+    //          .fillOpacity(1)
+    //          .stroke()
+    // }
+    // for (var p = 0; p < paraboxes.length; p++) {
+    //   pdfFile.rect(paraboxes[p][0],paraboxes[p][1],
+    //               paraboxes[p][2],paraboxes[p][3])
+    //          .strokeColor('#90EE90')
+    //          .lineWidth(0.5)
+    //          .fillColor('#90EE90')
+    //          .fillOpacity(0.5)
+    //          .dash(6, {space: 3})
+    //          .stroke()
+    // }
+    
+    pdfFile.image(data, 0, 0, {
+      fit: [newFile.width, newFile.height]
+    })
+    
+    pdfFile.end()
+  }
+)
